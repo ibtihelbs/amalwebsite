49492cb54ea0df3f4c097855f820a6865c99708d
ibtihelbs <91198460+ibtihelbs@users.noreply.github.com>
2022-10-03 11:32:05 +0200

    second

--- Components/Header.js
+++ Components/Header.js
@@ -1,30 +1,6 @@
 import Link from 'next/link'
-import { BlogContext } from '../Context/BlogContext'
-import NewPost from './NewPost'
-import { useContext } from 'react'
-import Modal from 'react-modal'
-import { useRouter } from 'next/router'
-Modal.setAppElement('#__next')
-
-const costumStyles = {
-  content: {
-    top: '50%',
-    left: '50%',
-    bottom: 'auto',
-    right: 'auto',
-    transform: 'translate(-50%, -50%)',
-    backgroundColor: '#fff',
-    padding:0,
-    border:'none'
-  },
-  overlay: {
-    backgroundColor: 'rgba(10, 11, 13, 0.69)',
-  }
-}
-
 const Header = () => {
-  const router = useRouter()
-  const {currentUser ,hundleAuth} = useContext(BlogContext)
+
   return (
     <ul className="flex justify-around gap-2">
       <li  className='self-center'>
@@ -46,22 +22,7 @@ const Header = () => {
             <h3>Contact</h3>
           </a>
       </li>
-      {!currentUser ? <li onClick={hundleAuth}>
-        sign in 
-      </li>: <>
-              <li className="underline">you're logged in</li>
-              <Link href="/?addNew=1">
-               <li  className='self-center'>write</li>
-              </Link> 
-             </>
-      }
-      <Modal
-       isOpen={Boolean(router.query.addNew)}
-       onRequestClose={()=> router.push('/')}
-       style={costumStyles}
-      >
-        <NewPost/>
-      </Modal>
+      
     </ul>
   )
   //()=> router.push('/')
